143516b913b1c0d6b577410fa8f32425d61bb5e0
mahm0ud-0 <mahmoudaborasheed2017@gmail.com>
2024-10-13 17:53:53 +0300

    bug fixed: Netlify not Building

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import logo from '../assets/logo.svg'
+import logo from '../assets/Logo.svg'
 import Button from './Button'
 
 function Navbar({ toggle, setToggle, closeMenu }) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],  theme: {
-    extend: {
-      colors: {
-        primaryWhite: '#FAFAFA',
-        secondary: '#F4CE14',
-        accent: '#495E57',
-        highlight: '#EE9972',
-        primaryBlack: '#333333'
-      }
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primaryWhite: "#FAFAFA",
+                secondary: "#F4CE14",
+                accent: "#495E57",
+                highlight: "#EE9972",
+                primaryBlack: "#333333",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
-
